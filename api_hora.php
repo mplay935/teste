febfc42d4b9126b601e88ab6847c7848a0ec0200
mplay935 <144961767+mplay935@users.noreply.github.com>
2024-05-24 19:11:46 -0400

    Update api_hora.php

--- api_hora.php
+++ api_hora.php
@@ -1,15 +1,1 @@
-<?php
-// Establece la cabecera de respuesta para indicar que el contenido es JSON
-header('Content-Type: application/json');
 
-// Obtiene la hora actual en formato ISO 8601
-$hora_actual = date('c');
-
-// Crea un array asociativo con la hora actual
-$respuesta = array(
-    'hora' => $hora_actual
-);
-
-// Convierte el array en formato JSON y lo imprime
-echo json_encode($respuesta);
-?>
